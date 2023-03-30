5afe3a6a00aa8a11991a0de1231d8a72f3490ff0
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-03-30 09:23:57 +0300

    02: MeetupAgenda: done

--- 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
@@ -1,23 +1,53 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    isTalk() {
+      return this.agendaItem.type === 'talk';
+    },
+  },
+
+  computed: {
+    data() {
+      const { type, title, startsAt, endsAt, description, speaker, language } = this.agendaItem;
+
+      return {
+        type,
+        title: title || agendaItemDefaultTitles[type],
+        icon: `/assets/icons/icon-${agendaItemIcons[type]}.svg`,
+        iconKey: agendaItemIcons[type],
+        time: `${startsAt} - ${endsAt}`,
+        description,
+        speaker,
+        language,
+      };
+    },
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="data.icon" class="icon" :alt="data.iconKey" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ data.time }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title">{{ data.title }}</h3>
+        <p v-if="isTalk()" class="agenda-item__talk">
+          <span>{{ data.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ data.language }}</span>
         </p>
-        <p>Description</p>
+        <p>{{ data.description }}</p>
       </div>
     </div>`,
 });
