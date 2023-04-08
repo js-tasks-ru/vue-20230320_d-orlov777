6bc3f590d17d5162eff3687e0d316fc8c27c07ea
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-08 14:36:00 +0300

    05: 02: done

--- 05-vue-router/02-NotFound/router/index.js
+++ 05-vue-router/02-NotFound/router/index.js
@@ -12,5 +12,9 @@ export const router = createRouter({
       path: '/page-b',
       component: () => import('../views/PageB'),
     },
+    {
+      path: '/:notFound(.*)*',
+      component: () => import('../views/PageNotFound'),
+    },
   ],
 });
