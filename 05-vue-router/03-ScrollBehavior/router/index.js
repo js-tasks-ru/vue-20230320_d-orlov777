553390547aee8fab20c296693cd692e3af38471c
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-09 11:30:20 +0300

    05: 03: done

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -3,6 +3,30 @@ import { createRouter, createWebHistory } from 'vue-router';
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
 
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+      };
+    }
+
+    if (from.meta?.saveScrollPosition && to.meta?.saveScrollPosition) {
+      return false;
+    }
+
+    if (savedPosition) {
+      return to.name === 'index'
+        ? new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(savedPosition);
+            }, 500);
+          })
+        : savedPosition;
+    }
+
+    return { left: 0, top: 0 };
+  },
+
   routes: [
     {
       path: '/',
