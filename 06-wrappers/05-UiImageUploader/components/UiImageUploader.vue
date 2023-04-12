d1b503efc3fc529da61d08f070c5d870cd0eeeef
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-12 18:10:47 +0300

    06: 05: done

--- 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
+++ 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
@@ -1,16 +1,114 @@
 <template>
   <div class="image-uploader">
-    <label class="image-uploader__preview image-uploader__preview-loading" style="--bg-url: url('/link.jpeg')">
-      <span class="image-uploader__text">Загрузить изображение</span>
-      <input type="file" accept="image/*" class="image-uploader__input" />
+    <label
+      class="image-uploader__preview"
+      :class="{ 'image-uploader__preview-loading': state === $options.StateType.UPLOADING }"
+      :style="state === $options.StateType.UPLOADED ? `--bg-url: url('${image}')` : ''"
+      @click="clickHandler"
+    >
+      <span class="image-uploader__text">{{ title }}</span>
+      <input
+        type="file"
+        accept="image/*"
+        class="image-uploader__input"
+        v-bind="$attrs"
+        ref="input"
+        @change="changeHandler"
+      />
     </label>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'UiImageUploader',
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const StateType = {
+  UPLOADING: 'UPLOADING',
+  UPLOADED: 'UPLOADED',
+  EMPTY: 'EMPTY',
+} as const;
+
+type TData = {
+  state: (typeof StateType)[keyof typeof StateType];
+  image: string;
 };
+
+export default defineComponent({
+  name: 'UiImageUploader',
+  inheritAttrs: false,
+
+  StateType,
+
+  props: {
+    preview: String,
+    uploader: Function,
+  },
+
+  emits: ['select', 'upload', 'error', 'remove'],
+
+  data(): TData {
+    return {
+      state: this.preview ? StateType.UPLOADED : StateType.EMPTY,
+      image: this.preview || '',
+    };
+  },
+
+  computed: {
+    title(): string {
+      switch (this.state) {
+        case StateType.EMPTY:
+          return 'Загрузить изображение';
+
+        case StateType.UPLOADING:
+          return 'Загрузка...';
+
+        default:
+        case StateType.UPLOADED:
+          return 'Удалить изображение';
+      }
+    },
+
+    clickHandler() {
+      return this.state === StateType.UPLOADED ? this.removeHandler : undefined;
+    },
+  },
+
+  methods: {
+    removeHandler(event: MouseEvent) {
+      event.preventDefault();
+      this.state = StateType.EMPTY;
+      (this.$refs.input as HTMLInputElement).value = '';
+      this.$emit('remove');
+    },
+
+    async changeHandler(event: Event) {
+      const { files } = event.target as HTMLInputElement;
+      if (!files) {
+        return;
+      }
+
+      this.image = URL.createObjectURL(files[0]);
+      this.$emit('select', files[0]);
+
+      if (!this.uploader) {
+        this.state = StateType.UPLOADED;
+        return;
+      }
+
+      const storedState = this.state;
+      this.state = StateType.UPLOADING;
+      try {
+        const response = await this.uploader(files[0]);
+        this.state = StateType.UPLOADED;
+        this.$emit('upload', response);
+      } catch (err) {
+        (this.$refs.input as HTMLInputElement).value = '';
+        this.state = storedState;
+        this.$emit('error', err);
+      }
+    },
+  },
+});
 </script>
 
 <style scoped>
