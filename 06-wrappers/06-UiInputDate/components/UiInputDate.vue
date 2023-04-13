4f8671e08acb7b3538bdd5a1b86410a806d2326e
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-13 12:58:32 +0300

    06: 06: done

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,101 @@
 <template>
-  <UiInput />
+  <UiInput v-model="computedModelValue" :type="type" :step="stepValue" ref="input">
+    <template v-if="$slots['left-icon']" #left-icon>
+      <slot name="left-icon" />
+    </template>
+    <template v-if="$slots['right-icon']" #right-icon>
+      <slot name="right-icon" />
+    </template>
+  </UiInput>
 </template>
 
-<script>
+<script lang="ts">
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import { defineComponent } from 'vue';
 import UiInput from './UiInput.vue';
 
-export default {
+dayjs.extend(utc);
+
+const DateKind = {
+  DATE: 'date',
+  TIME: 'time',
+  DATETIME_LOCAL: 'datetime-local',
+} as const;
+
+export default defineComponent({
   name: 'UiInputDate',
+  props: {
+    modelValue: Number,
+    type: {
+      type: String,
+      default: DateKind.DATE,
+      validate(value: string): boolean {
+        return value in DateKind;
+      },
+    },
+    step: {
+      type: Number,
+      default: 1,
+    },
+  },
+
+  emits: ['update:modelValue'],
 
   components: { UiInput },
-};
+
+  computed: {
+    dateValue(): number {
+      return dayjs(this.modelValue).utc().startOf('day').valueOf();
+    },
+
+    timeValue(): number {
+      return dayjs(this.modelValue).utc().valueOf() - this.dateValue;
+    },
+
+    secondValue(): number {
+      return dayjs(this.modelValue).utc().diff(dayjs(this.modelValue).utc().startOf('minute'), 'milliseconds');
+    },
+
+    stepValue(): number {
+      return this.type === DateKind.TIME ? 60 * this.step : this.step;
+    },
+
+    computedModelValue: {
+      get(): string {
+        if (!this.modelValue) {
+          return '';
+        }
+
+        switch (this.type) {
+          case DateKind.TIME:
+            return dayjs(this.modelValue).utc().format('HH:mm');
+
+          case DateKind.DATETIME_LOCAL:
+            return dayjs(this.modelValue).utc().format('YYYY-MM-DDTHH:mm:ss');
+
+          default:
+          case DateKind.DATE:
+            return dayjs(this.modelValue).utc().format('YYYY-MM-DD');
+        }
+      },
+
+      set() {
+        let inputValue: number = ((this.$refs.input as typeof UiInput).$refs.input as HTMLInputElement).valueAsNumber;
+
+        switch (this.type) {
+          case DateKind.TIME:
+            inputValue += this.dateValue + this.secondValue;
+            break;
+
+          case DateKind.DATE:
+            inputValue += this.timeValue;
+            break;
+        }
+
+        this.$emit('update:modelValue', inputValue);
+      },
+    },
+  },
+});
 </script>
