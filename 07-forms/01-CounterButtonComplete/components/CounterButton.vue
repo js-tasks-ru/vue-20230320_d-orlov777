ba6564d995dbb46b358c5a55f569aeea028c3415
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-14 10:44:53 +0300

    07: 01: done

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,39 @@
 <template>
-  <button></button>
+  <button @click="handleClick">{{ state }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      state: this.count,
+    };
+  },
+
+  watch: {
+    count(newValue) {
+      if (this.state !== newValue) {
+        this.state = newValue;
+      }
+    },
+  },
+
+  methods: {
+    handleClick() {
+      this.state += 1;
+      this.$emit('update:count', this.state);
+    },
+  },
 };
 </script>
