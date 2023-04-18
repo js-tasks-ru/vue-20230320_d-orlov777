f2d3d663c581df7a2f95bd47fb44e99ab66aee4d
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-18 21:45:20 +0300

    08: 04: done

--- 08-reactivity/04-debouncedRef/utils/debouncedRef.js
+++ 08-reactivity/04-debouncedRef/utils/debouncedRef.js
@@ -8,7 +8,18 @@ import debounce from 'lodash/debounce';
  * @returns {Ref<T>} - Новый ref, обновляющийся с debounce при обновлении исходного ref-а
  */
 export function debouncedRef(source, wait) {
-  const debounced = ref(undefined); // ...
-  // ...
+  const debounced = ref(source.value);
+
+  watch(
+    source,
+    debounce(
+      (newValue) => {
+        debounced.value = newValue;
+      },
+      wait,
+      { leading: true },
+    ),
+  );
+
   return debounced;
 }
