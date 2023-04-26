97b40ff5e78a415c5e7ce6e11884a7ee4b88663e
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-26 12:00:12 +0300

    10: 02: done

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{ state }}</h1>
+  <component v-if="component" :is="component" :result="result" :error="error" />
 </template>
 
 <script lang="ts">
@@ -7,12 +7,14 @@ import { defineComponent } from 'vue';
 
 const StateType = {
   PENDING: 'pending',
-  FULLFILLED: 'fullfilled',
+  FULFILLED: 'fulfilled',
   REJECTED: 'rejected',
 } as const;
 
 type TData = {
   state: (typeof StateType)[keyof typeof StateType];
+  result: any;
+  error: Error | null;
 };
 
 export default defineComponent({
@@ -30,16 +32,39 @@ export default defineComponent({
   data(): TData {
     return {
       state: StateType.PENDING,
+      error: null,
+      result: null,
     };
+  },
+
+  computed: {
+    component() {
+      switch (this.state) {
+        case StateType.PENDING:
+          return this.$slots.pending;
+        case StateType.FULFILLED:
+          return this.$slots.fulfilled;
+        case StateType.REJECTED:
+          return this.$slots.rejected;
+        default:
+          return null;
+      }
+    },
   },
 
   watch: {
     promise: {
-      handler() {
-        this.state = StateType.PENDING;
-        this.promise.then(() => (this.state = StateType.FULLFILLED)).catch(() => (this.state = StateType.REJECTED));
-      },
       immediate: true,
+      async handler() {
+        this.state = StateType.PENDING;
+        try {
+          this.result = await this.promise;
+          this.state = StateType.FULFILLED;
+        } catch (err) {
+          this.error = err as Error;
+          this.state = StateType.REJECTED;
+        }
+      },
     },
   },
 });
