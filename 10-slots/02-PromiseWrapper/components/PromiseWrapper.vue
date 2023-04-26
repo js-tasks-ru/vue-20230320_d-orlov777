8c4bcb0d97a9fda598f35ca7ae91928ebd0f7357
Dmtriy Orlov <jeth@Dima-Work.localdomain>
2023-04-26 11:35:32 +0300

    10: 02: handle promise

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,10 +1,24 @@
 <template>
-  <!-- -->
+  <h1>{{ state }}</h1>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const StateType = {
+  PENDING: 'pending',
+  FULLFILLED: 'fullfilled',
+  REJECTED: 'rejected',
+} as const;
+
+type TData = {
+  state: (typeof StateType)[keyof typeof StateType];
+};
+
+export default defineComponent({
   name: 'PromiseWrapper',
+
+  StateType,
 
   props: {
     promise: {
@@ -12,5 +26,21 @@ export default {
       required: true,
     },
   },
-};
+
+  data(): TData {
+    return {
+      state: StateType.PENDING,
+    };
+  },
+
+  watch: {
+    promise: {
+      handler() {
+        this.state = StateType.PENDING;
+        this.promise.then(() => (this.state = StateType.FULLFILLED)).catch(() => (this.state = StateType.REJECTED));
+      },
+      immediate: true,
+    },
+  },
+});
 </script>
